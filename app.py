8c818674438bd6e62d092ce10405e2727d1faa51
Tanimun Hasan <tanimunhasanemon@gmail.com>
2026-03-24 12:13:13 +0000

    Fix: improve project

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Check before run: add data files before run
 # Make sure file format is accepted by script algorithm
 # N2O sensors data fill in exel down to bottom
+# After running the script, copy the IP and open it in browser
 import base64
 import io
 from pathlib import Path
